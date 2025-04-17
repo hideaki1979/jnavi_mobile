4a87f5a5c4440ffdbe9e5be72b9f9550fe318e9e
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-04-17 21:02:45 +0900

    サインアップ機能の実装完了（パスワード・Google認証）

--- app.config.ts
+++ app.config.ts
@@ -3,51 +3,61 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-    ...config,
-    name: config.name ?? "jNavi",
-    slug: config.slug ?? "jNavi",
-    scheme: config.scheme ?? "jnavi",
-    android: {
-        ...config.android,
-        // 既に app.json に記載がある場合も上書きしないようにする
-        config: {
-            ...config.android?.config,
-            googleMaps: {
-                apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ?? "YOUR_FALLBACK_KEY"
-            }
-        },
-        googleServicesFile: "./google-services.json"
-    },
-    ios: {
-        ...config.ios?.config,
-        bundleIdentifier: config.ios?.bundleIdentifier,
-        config: {
-            googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ?? "YOUR_FALLBACK_KEY"
-        },
-        googleServicesFile: "./GoogleService-Info.plist"
-    },
-    extra: {
-        ...config.extra,
-        apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:3000"
-    },
-    plugins: [
-        [
-            "expo-router",
-            {
-                root: "./src/app"
-            }
-        ],
-        "@react-native-firebase/app",
-        "@react-native-firebase/auth",
-        "@react-native-google-signin/google-signin",
-        [
-            "expo-build-properties",
-            {
-                ios: {
-                    useFrameworks: "static"
+export default ({ config }: ConfigContext): ExpoConfig => {
+    // EASで定義しているシークレット環境変数
+    const googleServicesFileContent = process.env.GOOGLE_SERVICES_JSON
+    const googleServicesPlistContent = process.env.GOOGLE_SERVICES_PLIST
+
+    return {
+        ...config,
+        name: config.name ?? "jNavi",
+        slug: config.slug ?? "jNavi",
+        scheme: config.scheme ?? "jnavi",
+        android: {
+            ...config.android,
+            // 既に app.json に記載がある場合も上書きしないようにする
+            config: {
+                ...config.android?.config,
+                googleMaps: {
+                    apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ?? "YOUR_FALLBACK_KEY"
                 }
-            }
+            },
+            googleServicesFile: googleServicesFileContent
+                ? googleServicesFileContent
+                : "./google-services.json"
+        },
+        ios: {
+            ...config.ios?.config,
+            bundleIdentifier: config.ios?.bundleIdentifier,
+            config: {
+                googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ?? "YOUR_FALLBACK_KEY"
+            },
+            googleServicesFile: googleServicesPlistContent
+                ? googleServicesPlistContent
+                : "./GoogleService-Info.plist"
+        },
+        extra: {
+            ...config.extra,
+            apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:3000"
+        },
+        plugins: [
+            [
+                "expo-router",
+                {
+                    root: "./src/app"
+                }
+            ],
+            "@react-native-firebase/app",
+            "@react-native-firebase/auth",
+            "@react-native-google-signin/google-signin",
+            [
+                "expo-build-properties",
+                {
+                    ios: {
+                        useFrameworks: "static"
+                    }
+                }
+            ]
         ]
-    ]
-})
+    }
+}
